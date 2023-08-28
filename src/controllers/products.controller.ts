e815a92113dcdb47e1f8feb5dcf08966a9a5abec
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-28 12:00:24 -0300

    feat: implementing controllers and services on app module

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common'
 import { ProductsService } from '../services/products.service';
 
 @Controller('products')
-export class ProductController {
+export class ProductsController {
   constructor(private readonly productService: ProductsService) {}
 
   @Post()
