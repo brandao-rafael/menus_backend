212b2af81e3234951432a0485aa5730720f37dfa
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-28 16:43:08 -0300

    feat: return correct menu depending on the time

--- src/services/menu.service.ts
+++ src/services/menu.service.ts
@@ -7,4 +7,11 @@ export class MenuService extends CrudService {
   constructor(protected prisma: PrismaService) {
     super(prisma.menu);
   }
+
+  async findAll() {
+    const time = new Date();
+    const isNight = time.getHours() >= 18 || time.getHours() <= 6;
+    return this.prisma.menu.findMany({ include: { products: true }, where: { name: isNight ? 'Noturno' : 'Diurno' }});
+  }
+
 }
