de23543f3e3413e9dd4b7b5c500b3ae7b23d9294
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-28 11:46:32 -0300

    feat: create crud in categories service

---
+++ src/services/categories.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class CategoriesService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(payload: Prisma.CategoryCreateInput) {
+    return await this.prisma.category.create({
+      data: payload,
+    });
+  }
+
+  async findAll() {
+    return this.prisma.category.findMany();
+  }
+
+  async findOne(id: string) {
+    return this.prisma.category.findUnique({ where: { id } });
+  }
+
+  async update(id: string, data: Prisma.CategoryUpdateInput) {
+    return this.prisma.category.update({ where: { id }, data });
+  }
+
+  async remove(id: string) {
+    return this.prisma.category.delete({ where: { id } });
+  }
+
+}
