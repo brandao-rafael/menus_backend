c326b2bc0a5cdb64d85cdc251eff9db3a02da035
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-28 14:39:35 -0300

    refactor: create a abstract class to implements crud service

---
+++ src/services/crud.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export abstract class CrudService {
+  constructor(protected model: any) {}
+
+  async create(payload: any) {
+    return await this.model.create({
+      data: payload,
+    });
+  }
+
+  async findAll() {
+    try {
+    return this.model.findMany();
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+  async findOne(id: string) {
+    return this.model.findUnique({ where: { id } });
+  }
+
+  async update(id: string, data: any) {
+    return this.model.update({ where: { id }, data });
+  }
+
+  async remove(id: string) {
+    return this.model.delete({ where: { id } });
+  }
+}
