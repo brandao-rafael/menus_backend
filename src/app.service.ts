8305bf791d8d331397bf7f18105aa4969c49f77e
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-29 11:32:33 -0300

    feat: return the routes in /

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
 
+const hello: string = `
+API ROUTES
+
+Products
+- GET https://menus-backend.vercel.app/products/
+- GET https://menus-backend.vercel.app/products/'id'
+- POST https://menus-backend.vercel.app/products/
+- PUT https://menus-backend.vercel.app/products/'id' 
+- DELETE https://menus-backend.vercel.app/products/'id'
+
+Menu
+- GET https://menus-backend.vercel.app/menu/
+- GET https://menus-backend.vercel.app/menu/'id'
+- POST https://menus-backend.vercel.app/menu/
+- PUT https://menus-backend.vercel.app/menu/'id'
+- DELETE https://menus-backend.vercel.app/menu/'id'
+
+Category
+- GET https://menus-backend.vercel.app/category/
+- GET https://menus-backend.vercel.app/category/'id'
+- POST https://menus-backend.vercel.app/category/
+- PUT https://menus-backend.vercel.app/category/'id'
+- DELETE https://menus-backend.vercel.app/category/'id'
+`;
+
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return hello;
   }
 }
