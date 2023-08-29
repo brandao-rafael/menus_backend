f2556342fa37e5c277c6c260e490a0a6609a1a5a
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-29 11:47:08 -0300

    refactor: fromat return of /

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,28 +1,31 @@
 import { Injectable } from '@nestjs/common';
 
 const hello: string = `
-API ROUTES\n
+<h1>API ROUTES</h1>
 
-Products\n
-- GET https://menus-backend.vercel.app/products/\n
-- GET https://menus-backend.vercel.app/products/'id'\n
-- POST https://menus-backend.vercel.app/products/\n
-- PUT https://menus-backend.vercel.app/products/'id' \n
-- DELETE https://menus-backend.vercel.app/products/'id'\n
+<ul>Products
+<li>GET https://menus-backend.vercel.app/products/</li>
+<li>GET https://menus-backend.vercel.app/products/'id'</li>
+<li>POST https://menus-backend.vercel.app/products/</li>
+<li>PUT https://menus-backend.vercel.app/products/'id'</li>
+<li>DELETE https://menus-backend.vercel.app/products/'id'</li>
+</ul>
 
-Menu\n
-- GET https://menus-backend.vercel.app/menu/\n
-- GET https://menus-backend.vercel.app/menu/'id'\n
-- POST https://menus-backend.vercel.app/menu/\n
-- PUT https://menus-backend.vercel.app/menu/'id'\n
-- DELETE https://menus-backend.vercel.app/menu/'id'\n
+<ul>Menu
+<li>GET https://menus-backend.vercel.app/menu/</li>
+<li>GET https://menus-backend.vercel.app/menu/'id'</li>
+<li>POST https://menus-backend.vercel.app/menu/</li>
+<li>PUT https://menus-backend.vercel.app/menu/'id'</li>
+<li>DELETE https://menus-backend.vercel.app/menu/'id'</li>
+</ul>
 
-Category\n
-- GET https://menus-backend.vercel.app/category/\n
-- GET https://menus-backend.vercel.app/category/'id'\n
-- POST https://menus-backend.vercel.app/category/\n
-- PUT https://menus-backend.vercel.app/category/'id'\n
-- DELETE https://menus-backend.vercel.app/category/'id'\n
+<ul>Category
+<li>GET https://menus-backend.vercel.app/category/</li>
+<li>GET https://menus-backend.vercel.app/category/'id'</li>
+<li>POST https://menus-backend.vercel.app/category/</li>
+<li>PUT https://menus-backend.vercel.app/category/'id'</li>
+<li>DELETE https://menus-backend.vercel.app/category/'id'</li>
+</ul>
 `;
 
 @Injectable()
