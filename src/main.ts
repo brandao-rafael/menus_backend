e5452456b648c9e64604d7870dd438f6c8c1d5ce
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-29 09:28:50 -0300

    feat: change main to wornk in production

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import express from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
-  await app.listen(3001);
-}
-bootstrap();
+const server = express();
+
+const startNestApplication = async (expressInstance: any) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  return app.init();
+};
+
+startNestApplication(server)
+  .then(v => console.log('Nest Ready'))
+  .catch(err => console.error('Nest broken', err));
+
+export const app = server;
